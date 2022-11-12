5a02fb22360a4a6d035da03d3a2ea2bd12c5f40a
LamTien2003 <ltt209514@gmail.com>
2022-11-12 21:52:35 +0700

    Tien' commit lan 1

---
+++ js/app.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function(){
+    $('.slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: true,
+        fade: true,
+        speed: 1000,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa-solid fa-chevron-left'></i></button>",
+        nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa-solid fa-chevron-right'></i></button>"
+    });
+  });
