93f6676bf0127bde2408025f99cd41676224fe61
QuocLeAnh <quocleanh2003@gmail.com>
2022-11-18 16:01:11 +0700

    hello

--- js/app.js
+++ js/app.js
@@ -1,19 +1,4 @@
-// Password Hidden
-const input = document.querySelector(".input-pass");
-const eyeOpen = document.querySelector(".eye-open");
-const eyeClose = document.querySelector(".eye-close");
 
-eyeOpen.addEventListener("click", function () {
-  eyeOpen.classList.add("hidden");
-  eyeClose.classList.remove("hidden");
-  input.setAttribute("type", "password");
-});
-
-eyeClose.addEventListener("click", function () {
-  eyeOpen.classList.remove("hidden");
-  eyeClose.classList.add("hidden");
-  input.setAttribute("type", "text");
-});
 
 $(document).ready(function(){
     $('.slider').slick({
